d0d29d6f581b9011cfb6eb10d329c81acce6a443
Markus Delgado <m1delgado@ucsd.edu>
2024-03-05 15:14:02 -0800

    #49, #68, #69, #70, Fixed ItemListFragment (only pulls up todays items), Edited TomorrowListFragment (can now add items to tomorrow), created, CreateTomorrowItemDialog

--- app/src/main/java/edu/ucsd/cse110/successorator/ui/itemList/TomorrowListFragment.java
+++ app/src/main/java/edu/ucsd/cse110/successorator/ui/itemList/TomorrowListFragment.java
@@ -4,28 +4,34 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import java.time.ZonedDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import edu.ucsd.cse110.successorator.DateFormatter;
 import edu.ucsd.cse110.successorator.MainViewModel;
 import edu.ucsd.cse110.successorator.databinding.FragmentCardListBinding;
 import edu.ucsd.cse110.successorator.databinding.FragmentTomorrowListBinding;
 import edu.ucsd.cse110.successorator.ui.itemList.dialog.CreateItemDialogFragment;
+import edu.ucsd.cse110.successorator.ui.itemList.dialog.CreateTomorrowItemDialogFragment;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link TomorrowListFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class TomorrowListFragment extends Fragment {
+public class TomorrowListFragment extends ParentFragment {
     private MainViewModel activityModel;
     private FragmentTomorrowListBinding view;
     private ItemListAdapter adapter;
@@ -56,6 +62,33 @@ public class TomorrowListFragment extends Fragment {
         var modelProvider = new ViewModelProvider(modelOwner, modelFactory);
         this.activityModel = modelProvider.get(MainViewModel.class);
 
+        this.adapter = new ItemListAdapter(requireContext(), List.of(), activityModel::remove, activityModel::append, activityModel::prepend, activityModel::markCompleteOrIncomplete, "HOME");
+
+
+        activityModel.getOrderedCards().observe(cards -> {
+            if(cards == null) return;
+            adapter.clear();
+
+            for(int i = 0; i < cards.size(); i++){
+                if(cards.get(i).getDate().getDayOfMonth() == ZonedDateTime.now().plusDays(1).getDayOfMonth()){
+                    adapter.add(cards.get(i));
+                }
+            }
+
+            adapter.addAll(new ArrayList<>(cards));
+            adapter.notifyDataSetChanged();
+            for(int i = 0; i < cards.size(); i++) {
+                Log.d("Ordered cards changed", cards.get(i).sortOrder() + " " + i + " " + cards.get(i).getDescription());
+            }
+
+            if(activityModel.size() != 0 && view != null){
+                view.placeholderText.setVisibility(View.GONE);
+            }
+            if(activityModel.size() == 0 && view != null){
+                view.placeholderText.setVisibility(View.VISIBLE);
+            }
+        });
+
         dateFormatter = new DateFormatter(ZonedDateTime.now());
 
 
@@ -66,6 +99,7 @@ public class TomorrowListFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         this.view = FragmentTomorrowListBinding.inflate(inflater, container, false);
+        view.cardList.setAdapter(adapter);
         dateText = this.view.dateView;
 
 
@@ -87,9 +121,9 @@ public class TomorrowListFragment extends Fragment {
 
 
         view.addItem.setOnClickListener(v ->{
-            var dialogFragment = CreateItemDialogFragment.newInstance();
+            var dialogFragment = CreateTomorrowItemDialogFragment.newInstance();
             // Unsure if we should use getSupportFragmentManager() or getParentFragmentManager()
-            dialogFragment.show(getParentFragmentManager(),"CreateItemDialogFragment");
+            dialogFragment.show(getParentFragmentManager(),"CreateTomorrowItemDialogFragment");
         });
 
         // When pressing the add date button, the Date will advance by 24hrs
@@ -110,8 +144,45 @@ public class TomorrowListFragment extends Fragment {
 
         return view.getRoot();
     }
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState){
+        activityModel.getOrderedCards().observe(cards -> {
+            if(cards == null) return;
+            adapter.clear();
+            for(int i = 0; i < cards.size(); i++){
+                if(cards.get(i).getDate().getDayOfMonth() == ZonedDateTime.now().plusDays(1).getDayOfMonth()){
+                    adapter.add(cards.get(i));
+                }
+            }
+            //adapter.addAll(new ArrayList<>(cards));
+            adapter.notifyDataSetChanged();
+            for(int i = 0; i < cards.size(); i++) {
+                Log.d("Ordered cards changed", cards.get(i).sortOrder() + " " + i + " " + cards.get(i).getDescription());
+            }
+
+            if(activityModel.size() != 0){
+                this.view.placeholderText.setVisibility(View.GONE);
+            }
+            if(activityModel.size() == 0){
+                this.view.placeholderText.setVisibility(View.VISIBLE);
+            }
+        });
+    }
     public void onResume() {
         super.onResume();
+        // Get formatted date and display.
+        String savedDate = sharedPreferences.getString("formatted_date", "ERR");
+
+        // Check for date changes
+        if (!(dateFormatter.getTodaysDate(ZonedDateTime.now()).equals(savedDate))) {
+            activityModel.removeAllComplete();
+            formattedDate = dateFormatter.getTomorrowsDate(ZonedDateTime.now());
+
+            // Edit date persistence
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putString("formatted_date", formattedDate);
+            editor.apply();
+        }
+
         dateText.setText(sharedPreferences.getString("formatted_date", "ERR"));
     }
 }
---
+++ app/src/main/java/edu/ucsd/cse110/successorator/ui/itemList/dialog/CreateTomorrowItemDialogFragment.java
@@ -0,0 +1,116 @@
+package edu.ucsd.cse110.successorator.ui.itemList.dialog;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.time.ZonedDateTime;
+
+import edu.ucsd.cse110.successorator.DateFormatter;
+import edu.ucsd.cse110.successorator.MainViewModel;
+import edu.ucsd.cse110.successorator.R;
+import edu.ucsd.cse110.successorator.databinding.FragmentDialogAddItemBinding;
+import edu.ucsd.cse110.successorator.lib.domain.Item;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link CreateTomorrowItemDialogFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class CreateTomorrowItemDialogFragment extends DialogFragment {
+
+    private FragmentDialogAddItemBinding view;
+
+    private MainViewModel activityModel;
+
+    private DateFormatter dateFormatter;
+
+    CreateTomorrowItemDialogFragment() {
+        // Empty required constructor
+    }
+
+    public static CreateTomorrowItemDialogFragment newInstance() {
+        var fragment = new CreateTomorrowItemDialogFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        ZonedDateTime curr = ZonedDateTime.now();
+        dateFormatter = new DateFormatter(curr);
+
+        this.view = FragmentDialogAddItemBinding.inflate(getLayoutInflater());
+        view.NONE.setChecked(true);
+        // Set the text for radio buttons
+        view.WEEKLY.setText("Weekly on " + dateFormatter.weeklyDate(curr));
+        view.MONTHLY.setText("Monthly " + dateFormatter.monthlyDate(curr));
+        view.YEARLY.setText("Yearly on " + dateFormatter.yearlyDate(curr));
+        return new AlertDialog.Builder(getActivity())
+                .setTitle("New Item")
+                .setMessage("Please enter your MIT")
+                .setView(view.getRoot())
+                .setPositiveButton("Create", this::onPositiveButtonClick)
+                .setNegativeButton("Cancel", this::onNegativeButtonClick)
+                .create();
+    }
+
+    private void onPositiveButtonClick(DialogInterface dialog, int which) {
+        var description = view.editTextDialog.getText().toString();
+
+        var item = makeRecurrenceItem(description);
+        activityModel.append(item);
+        dialog.dismiss();
+    }
+
+    private void onNegativeButtonClick(DialogInterface dialog, int which) {
+        dialog.cancel();
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+
+        var modelOwner = requireActivity();
+        var modelFactory = ViewModelProvider.Factory.from(MainViewModel.initializer);
+        var modelProvider = new ViewModelProvider(modelOwner, modelFactory);
+        this.activityModel = modelProvider.get(MainViewModel.class);
+    }
+    private Item makeRecurrenceItem(String description){
+        Item returnitem;
+        if (view.NONE.isChecked()){
+            returnitem = new Item(description, null, -1, false,
+                    ZonedDateTime.now().plusDays(1), false, "NONE", false);
+        } else if (view.DAILY.isChecked()){
+            description += ", daily";
+            returnitem = new Item(description, null, -1, false,
+                    ZonedDateTime.now().plusDays(1), true, "DAILY",false );
+        } else if (view.WEEKLY.isChecked()){
+            description += ", weekly on " +  ZonedDateTime.now().getDayOfWeek().toString();
+            returnitem = new Item(description, null, -1, false,
+                    ZonedDateTime.now().plusDays(1), true, "WEEKLY",false);
+        } else if (view.MONTHLY.isChecked()){
+            description += ", monthly on " +  dateFormatter.monthlyDate(ZonedDateTime.now());
+            returnitem = new Item(description, null, -1, false,
+                    ZonedDateTime.now().plusDays(1), true, "MONTHLY",false);
+        } else {
+            description += ", yearly on " +  dateFormatter.yearlyDate(ZonedDateTime.now());
+            returnitem = new Item(description, null, -1, false,
+                    ZonedDateTime.now().plusDays(1), true, "YEARLY",false);
+        }
+        return returnitem;
+    }
+}
