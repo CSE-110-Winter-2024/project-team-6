d9d631fafacba914d710270e25756f8a2c5899cf
Markus Delgado <m1delgado@ucsd.edu>
2024-03-11 14:28:46 -0700

    #46 testing MainViewModel all Methods

--- app/src/test/java/edu/ucsd/cse110/successorator/MainViewModelTest.java
+++ app/src/test/java/edu/ucsd/cse110/successorator/MainViewModelTest.java
@@ -50,4 +50,115 @@ public class MainViewModelTest extends TestCase {
             assertEquals("Item 3", cards.get(2).getDescription());
         });
     }
+
+    @Test
+    public void testAppend(){
+        List<Item> items = new ArrayList<>();
+        items.add(new Item("Item 1", 1, 1, false,
+                mockTime, false, "NONE", false, false, "NONE"));
+        items.add(new Item("Item 2", 2, 2, false,
+                mockTime, false, "NONE", false, false, "NONE"));
+        items.add(new Item("Item 3", 3, 3, false,
+                mockTime, false, "NONE", false, false, "NONE"));
+        dataSource.putItems(items);
+
+
+        MainViewModel viewModel = new MainViewModel(itemRepository);
+
+        viewModel.append(new Item("Item 4", 4, 4, false,
+                mockTime, false, "NONE", false, false, "NONE"));
+
+        viewModel.getOrderedCards().observe(cards -> {
+            assertEquals(4, cards.size());
+            assertEquals("Item 1", cards.get(0).getDescription());
+            assertEquals("Item 2", cards.get(1).getDescription());
+            assertEquals("Item 3", cards.get(2).getDescription());
+            assertEquals("Item 4", cards.get(3).getDescription());
+        });
+    }
+    @Test
+    public void testPrepend() {
+        List<Item> items = new ArrayList<>();
+        items.add(new Item("Item 1", 1, 1, false,
+                mockTime, false, "NONE", false, false, "NONE"));
+        items.add(new Item("Item 2", 2, 2, false,
+                mockTime, false, "NONE", false, false, "NONE"));
+        items.add(new Item("Item 3", 3, 3, false,
+                mockTime, false, "NONE", false, false, "NONE"));
+        dataSource.putItems(items);
+
+        MainViewModel viewModel = new MainViewModel(itemRepository);
+
+        viewModel.prepend(new Item("Item 4", 4, 4, false,
+                mockTime, false, "NONE", false, false, "NONE"));
+        viewModel.getOrderedCards().observe(cards -> {
+            assertEquals(4, cards.size());
+            assertEquals("Item 1", cards.get(1).getDescription());
+            assertEquals("Item 2", cards.get(2).getDescription());
+            assertEquals("Item 3", cards.get(3).getDescription());
+            assertEquals("Item 4", cards.get(0).getDescription());
+        });
+    }
+    @Test
+    public void testRemove(){
+        List<Item> items = new ArrayList<>();
+        items.add(new Item("Item 1", 1, 1, false,
+                mockTime, false, "NONE", false, false, "NONE"));
+        items.add(new Item("Item 2", 2, 2, false,
+                mockTime, false, "NONE", false, false, "NONE"));
+        items.add(new Item("Item 3", 3, 3, false,
+                mockTime, false, "NONE", false, false, "NONE"));
+        dataSource.putItems(items);
+
+        MainViewModel viewModel = new MainViewModel(itemRepository);
+
+        viewModel.remove(2);
+        viewModel.getOrderedCards().observe(cards -> {
+            assertEquals(2, cards.size());
+            assertEquals("Item 1", cards.get(0).getDescription());
+            assertEquals("Item 3", cards.get(1).getDescription());
+
+        });
+    }
+    @Test
+    public void testSize(){
+        List<Item> items = new ArrayList<>();
+        items.add(new Item("Item 1", 1, 1, false,
+                mockTime, false, "NONE", false, false, "NONE"));
+        items.add(new Item("Item 2", 2, 2, false,
+                mockTime, false, "NONE", false, false, "NONE"));
+        items.add(new Item("Item 3", 3, 3, false,
+                mockTime, false, "NONE", false, false, "NONE"));
+        dataSource.putItems(items);
+
+        MainViewModel viewModel = new MainViewModel(itemRepository);
+
+        assertEquals(viewModel.size(), 3);
+    }
+    @Test
+    public void testRemoveAllComplete(){
+        List<Item> items = new ArrayList<>();
+        items.add(new Item("Item 1", 1, 1, true,
+                mockTime, false, "NONE", false, false, "NONE"));
+        items.add(new Item("Item 2", 2, 2, false,
+                mockTime, false, "NONE", false, false, "NONE"));
+        items.add(new Item("Item 3", 3, 3, true,
+                mockTime, false, "NONE", false, false, "NONE"));
+        items.add(new Item("Item 4", 4, 4, false,
+                mockTime, false, "NONE", false, false, "NONE"));
+        items.add(new Item("Item 5", 5, 5, true,
+                mockTime, false, "NONE", false, false, "NONE"));
+        dataSource.putItems(items);
+
+        MainViewModel viewModel = new MainViewModel(itemRepository);
+
+        viewModel.removeAllComplete();
+
+        viewModel.getOrderedCards().observe(cards -> {
+            assertEquals(2, cards.size());
+            assertEquals("Item 2", cards.get(0).getDescription());
+            assertEquals("Item 4", cards.get(1).getDescription());
+
+        });
+    }
 }
