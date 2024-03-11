ec074214f0f7cbffdf1b74bc35288a792ebf692f
Haochen Qiu <114204731+josephjo7star@users.noreply.github.com>
2024-03-10 17:10:03 -0700

    Added a Hamburger menu for focusing mode with a drawer layout; Unexpected Problem: cannot add goal nor in every fragment; Reference Website for code: https://www.geeksforgeeks.org/navigation-drawer-in-android/; Issue #45, #83

--- app/src/main/java/edu/ucsd/cse110/successorator/MainActivity.java
+++ app/src/main/java/edu/ucsd/cse110/successorator/MainActivity.java
@@ -3,9 +3,11 @@ package edu.ucsd.cse110.successorator;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
@@ -15,11 +17,10 @@ import edu.ucsd.cse110.successorator.ui.itemList.PendingListFragment;
 import edu.ucsd.cse110.successorator.ui.itemList.RecurringListFragment;
 import edu.ucsd.cse110.successorator.ui.itemList.TomorrowListFragment;
 
-import android.util.Log;
-
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding view;
-
+    public DrawerLayout drawerLayout;
+    public ActionBarDrawerToggle actionBarDrawerToggle;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -28,6 +29,17 @@ public class MainActivity extends AppCompatActivity {
 
         this.view = ActivityMainBinding.inflate(getLayoutInflater(), null, false);
         setContentView(view.getRoot());
+
+        drawerLayout = findViewById(R.id.my_drawer_layout);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.nav_open, R.string.nav_close);
+
+        // pass the Open and Close toggle for the drawer layout listener
+        // to toggle the button
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+
+        // to make the Navigation drawer icon always appear on the action bar
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
     }
 
@@ -47,22 +59,26 @@ public class MainActivity extends AppCompatActivity {
         // Handle item selection
         int id = item.getItemId();
 
-        if (id == R.id.action_today) {
+        if (id == R.id.today) {
             swapFragments(ItemListFragment.class);
             return true;
-        } else if (id == R.id.action_tomorrow) {
+        } else if (id == R.id.Tomorrow) {
             swapFragments(TomorrowListFragment.class);
             return true;
-        } else if (id == R.id.action_reccurring) {
+        } else if (id == R.id.Recurring) {
             swapFragments(RecurringListFragment.class);
             return true;
-        } else if (id == R.id.action_pending) {
+        } else if (id == R.id.Pending) {
             swapFragments(PendingListFragment.class);
             return true;
         }
         // Continue with other options...
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
 
         return super.onOptionsItemSelected(item);
+
     }
 
     private void swapFragments(Class<? extends Fragment> fragmentClass) {
