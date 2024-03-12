cfe73f6156369c62e1eb8c0baf20d9430280eb23
edolosa <edolosa@ucsd.edu>
2024-03-12 14:16:52 -0700

    #80 readded grey out when done

--- app/src/main/java/edu/ucsd/cse110/successorator/ui/itemList/ItemListAdapter.java
+++ app/src/main/java/edu/ucsd/cse110/successorator/ui/itemList/ItemListAdapter.java
@@ -91,19 +91,43 @@ public class ItemListAdapter extends ArrayAdapter<Item> {
         // Home by default
         // binding.TAG.setBackground(ContextCompat.getDrawable(super.getContext(), R.drawable.outline_home));
 
+        if (!flashcard.isDone()) {
+            switch (flashcard.getCategory()) {
+                case "HOME":
+                    binding.TAG.setBackground(ContextCompat.getDrawable(super.getContext(), R.drawable.outline_home));
+                    binding.TAG.setText("H");
+                    break;
+                case "WORK":
+                    binding.TAG.setBackground(ContextCompat.getDrawable(super.getContext(), R.drawable.outline_work));
+                    binding.TAG.setText("W");
+                    break;
+                case "SCHOOL":
+                    binding.TAG.setBackground(ContextCompat.getDrawable(super.getContext(), R.drawable.outline_school));
+                    binding.TAG.setText("S");
+                    break;
+                case "ERRAND":
+                    binding.TAG.setBackground(ContextCompat.getDrawable(super.getContext(), R.drawable.outline_errands));
+                    binding.TAG.setText("E");
+                    break;
+            }
 
-        if (flashcard.getCategory().equals("HOME")) {
-            binding.TAG.setBackground(ContextCompat.getDrawable(super.getContext(), R.drawable.outline_home));
-            binding.TAG.setText("H");
-        } else if (flashcard.getCategory().equals("WORK")) {
-            binding.TAG.setBackground(ContextCompat.getDrawable(super.getContext(), R.drawable.outline_work));
-            binding.TAG.setText("W");
-        } else if (flashcard.getCategory().equals("SCHOOL")) {
-            binding.TAG.setBackground(ContextCompat.getDrawable(super.getContext(), R.drawable.outline_school));
-            binding.TAG.setText("S");
-        } else if (flashcard.getCategory().equals("ERRAND")) {
-            binding.TAG.setBackground(ContextCompat.getDrawable(super.getContext(), R.drawable.outline_errands));
-            binding.TAG.setText("E");
+        } else {
+            binding.TAG.setBackground(ContextCompat.getDrawable(super.getContext(), R.drawable.outline_done));
+            switch (flashcard.getCategory()) {
+                case "HOME":
+                    binding.TAG.setText("H");
+                    break;
+                case "WORK":
+                    binding.TAG.setText("W");
+                    break;
+                case "SCHOOL":
+                    binding.TAG.setText("S");
+                    break;
+                case "ERRAND":
+                    binding.TAG.setText("E");
+                    break;
+            }
+
         }
 
 
